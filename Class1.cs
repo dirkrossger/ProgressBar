e5ec819e9e4aa9f9cf93fd6847ccdbd736ba6650
dirk.roessger@gmail.com <dirk.roessger@gmail.com>
2017-08-24 18:29:35 +0200

    20170824

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ProgressMeterTest
+{
+    public class Cmds
+    {
+        [CommandMethod("PB")]
+        public void ProgressBarManaged()
+        {
+            ProgressMeter pm = new ProgressMeter();
+            pm.Start("Testing Progress Bar");
+            pm.SetLimit(100);
+            // Now our lengthy operation
+            for (int i = 0; i <= 100; i++)
+            {
+                System.Threading.Thread.Sleep(5);
+                // Increment Progress Meter...
+                pm.MeterProgress();
+                // This allows AutoCAD to repaint
+                Application.DoEvents();
+            }
+            pm.Stop();
+        }
+    }
+}
